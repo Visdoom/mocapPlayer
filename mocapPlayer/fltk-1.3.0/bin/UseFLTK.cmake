9a8d104090e41f3074cbaa8a56c2a4c8ee425fd9
sophie-thinkPad <SLaturnus@googlemail.com>
2013-11-26 18:38:44 +0100

    hopefully it works: commando line executable

--- mocapPlayer/fltk-1.3.0/bin/UseFLTK.cmake
+++
@@ -1,8 +0,0 @@
-#
-# automatically generated - do not edit
-
-include_directories("/usr/local/include")
-
-if(FLTK_EXE_LINKER_FLAGS)
-   list(APPEND CMAKE_EXE_LINKER_FLAGS "${FLTK_EXE_LINKER_FLAGS}")
-endif(FLTK_EXE_LINKER_FLAGS)
